fd6e07e5b8ad453eb931a10cc07ce74155907895
gerryfrank10 <gerryfrank720@gmail.com>
2026-01-10 22:41:06 +0000

    update for theme

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/index.css";
 import App from "@/App";
+import {AuthProvider} from "@/context/AuthContext";
+import {ThemeProvider} from "@/context/ThemeContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ThemeProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ThemeProvider>
+  </React.StrictMode>
 );
